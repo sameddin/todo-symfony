3a1b5ea1e7ac37c9aa19aabb8f96405a000215cd
Sameddin Pasizade <sameddin.pasizade@gmail.com>
2015-09-19 03:32:45 +0500

    Creating Part of CRUD

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,63 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        $db = $this->get('database_connection');
+        $tasks = $db->fetchAll('SELECT * FROM task');
+        return $this->render('default/home.html.twig', array(
+            'tasks' => $tasks,
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
         ));
+
+    }
+
+    /**
+     * @Route("/vasiliy-dobavlyaet-task", name="task.add")
+     */
+    public function addAction(Request $request)
+    {
+        $text = $request->request->get('text');
+        var_dump($text);
+
+
+        $db = $this->get('database_connection');
+
+        $db->insert('task', array('text' => $text));
+        $tasks = $db->fetchAll('SELECT * FROM task');
+
+        return $this->redirectToRoute('homepage');
+    }
+
+    /**
+     * @Route("/vasiliy-obnovlyaet-task", name="task.update")
+     */
+    public function updateAction($id)
+    {
+        $db = $this->getDoctrine()->getManager();
+        $tasks = $db->getRepository('text')->find($text);
+
+        return $this->redirectToRoute('homepage');
+    }
+
+    /**
+     * @Route("/vasiliy-udalyaet-task", name="task.delete")
+     */
+    public function deleteAction($text)
+    {
+        $db = $this->getDoctrine()->getManager();
+        $tasks = $db->getRepository('text')->find($text);
+
+        $db->remove($tasks);
+        $db->flush();
+    }
+
+    /**
+     * @Route("/vasiliy-pokazivaet-task", name="task.show")
+     */
+    public function showAction($text)
+    {
+        $tasks = $this->getDoctrine()
+            ->getRepository('text')
+            ->find($text);
+        return $this->render('default/index.html.twig',array('text' => $text));
     }
 }
